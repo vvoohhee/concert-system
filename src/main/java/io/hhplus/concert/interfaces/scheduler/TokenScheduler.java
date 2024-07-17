545072619955a4aa166cd652e31b6403f59482ed
woohee <whpark@autocrypt.io>
2024-07-18 01:27:49 +0900

    Feat : 로깅 필터 구현, 인터셉터/스케줄러 위치 변경

---
+++ src/main/java/io/hhplus/concert/interfaces/scheduler/TokenScheduler.java
@@ -0,0 +1,24 @@
+package io.hhplus.concert.interfaces.scheduler;
+
+import io.hhplus.concert.application.token.UserTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class TokenScheduler {
+    private final UserTokenService tokenService;
+
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
+    public void process() {
+        tokenService.process();
+    }
+
+    @Scheduled(fixedRate = 3, timeUnit = TimeUnit.MINUTES)
+    public void expire() {
+        tokenService.expire();
+    }
+}
