8ff458a0470b71fca1cd7fc20f8f5c6feaed2d05
woohee <whpark@autocrypt.io>
2024-07-26 09:44:22 +0900

    결제에 낙관적 락, 트랜잭션 어노테이션 적용방식 변경

---
+++ src/main/java/io/hhplus/concert/application/payment/UserPaymentComponent.java
@@ -0,0 +1,31 @@
+package io.hhplus.concert.application.payment;
+
+import io.hhplus.concert.domain.concert.dto.SeatPriceInfo;
+import io.hhplus.concert.domain.payment.PaymentService;
+import io.hhplus.concert.domain.payment.Ticket;
+import io.hhplus.concert.domain.user.UserService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class UserPaymentComponent {
+
+    private final UserService userService;
+    private final PaymentService paymentService;
+
+    @Transactional
+    public List<Ticket> updatePaymentData(Long userId, List<SeatPriceInfo> priceInfos) {
+        Integer totalPrice = 0;
+        for (SeatPriceInfo seatPriceInfo : priceInfos) totalPrice += seatPriceInfo.price();
+
+        userService.consumeBalance(userId, totalPrice);
+        List<Ticket> tickets = paymentService.billing(userId, priceInfos);
+        paymentService.savePaymentHistories(tickets);
+
+        return tickets;
+    }
+}
